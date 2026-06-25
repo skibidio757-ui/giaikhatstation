d6c1d2ef95d09ee63dcbd194d808f953576ad8d4
skibidio757-ui <skibidio757@gmail.com>
2026-06-25 19:20:39 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBuCLyl6W-mTp_P1ML-daoWqsVP0gWP4UI",
+  apiKey: "AIzaSyBuCLyl6W-mTp_P1NL-daoWqsVP0gWp4UI",
   authDomain: "giaikhatstaton.firebaseapp.com",
   projectId: "giaikhatstaton",
   storageBucket: "giaikhatstaton.firebasestorage.app",
@@ -7,3 +7,7 @@ const firebaseConfig = {
   appId: "1:292568980848:web:7a6fa596a321c5478943b5",
   measurementId: "G-DG9LQLGZS0"
 };
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
